8e5df1642638fc168d9568746461fff24c2b96a4
ugly-kid-af <dwarmachine24@gmail.com>
2022-12-18 05:54:40 +0000

    haydn: vendorsetup.sh: update vendor tree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@ git clone --depth=1 https://github.com/Alucard-Storm/kernel_xiaomi_haydn.git ker
 
 # Vendor
 rm -rf vendor/xiaomi/haydn
-git clone --depth=1 https://gitlab.com/omraj12/projectelixir_vendor_xiaomi_haydn vendor/xiaomi/haydn
+git clone --depth=1 https://github.com/omraj12/elixir-vendor-xiaomi-haydn.git vendor/xiaomi/haydn
 
 # Firmware
 rm -rf vendor/xiaomi/haydn-firmware
